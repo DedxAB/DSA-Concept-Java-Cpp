ce036f9567915d81b1eaac1e03e488f7181aedf9
Arnab Bhoumik <86946087+DedxAB@users.noreply.github.com>
2023-02-20 22:24:40 +0530

    Update CheckSortedOrNot.java

--- Recursion/CheckSortedOrNot.java
+++ Recursion/CheckSortedOrNot.java
@@ -3,8 +3,8 @@ package arnab.DSA.Recursion;
 public class CheckSortedOrNot {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 4, 8, 7 };
-        int size = 5;
-        boolean ans = isSortedArray(arr, 1, size);
+        // int size = 5;
+        boolean ans = isSortedArray(arr, 1, arr.length);
         if (ans) {
             System.out.println("Sorted");
         } else {
